9ceb5951771b8b771c9776ba32bcc4fb7745e9a4
katieb1 <birchardkatie@gmail.com>
2024-12-13 11:06:09 -0800

    Change rsyncrosim version check to 2.0.0

--- src/growth-pandora.R
+++ src/growth-pandora.R
@@ -26,7 +26,7 @@ checkPackageVersion <- function(packageString, minimumVersion){
   }
 }
 
-checkPackageVersion("rsyncrosim", "2.0.1")
+checkPackageVersion("rsyncrosim", "2.0.0")
 checkPackageVersion("tidyverse",  "2.0.0")
 checkPackageVersion("dplyr",      "1.1.2")
 checkPackageVersion("codetools",  "0.2.19")
